025b775f33bdaad52966e2fbad56a95d81b2e0dd
Hiroki <ore01100001binary@gmail.com>
2025-08-08 22:10:52 +0900

    フロントエンドの開発開始

---
+++ backend/src/main/java/com/example/observationcalendar/ObservationCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.observationcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservationCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservationCalendarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/observation_calendar/ObservationCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.observation_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservationCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
